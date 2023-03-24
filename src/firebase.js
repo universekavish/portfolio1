3ca1d4114518bdb176b0b471e5a00e65dac96d33
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-03-25 02:30:30 +0530

    hiding in env

--- src/firebase.js
+++ src/firebase.js
@@ -5,19 +5,12 @@ import 'firebase/compat/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 var firebaseApp = firebase.initializeApp({
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREBASE_APP_ID
-
-    apiKey: "AIzaSyCNrJxkj31HBPNYwA-Hc4zQz1p12bCidkA",
-    authDomain:"portfolio-kavish-729d6.firebaseapp.com",
-    projectId:"portfolio-kavish-729d6",
-    storageBucket:"portfolio-kavish-729d6.appspot.com",
-    messagingSenderId:"151716688191",
-    appId:"1:151716688191:web:ddde7412906606172ce38a"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 var db = firebaseApp.firestore();
