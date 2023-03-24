38bca6415bbda47bf17a7e236221211e2a209dc1
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-03-24 21:50:37 +0530

    adding .env to .gitignore

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,25 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import firebase from 'firebase';
 
 const Contact = () => {
+    addMessage(e) {
+        e.preventDefault();
+
+        var firebaseConfig = {
+            apiKey:process.REACT_APP_FIREBASE_API_KEY,
+            authDomain: "XXXXXXXXXXXXXXXXXX",
+            databaseURL: "XXXXXXXXXXXXXXXXXX",
+            projectId: "XXXXXXXXXXXXXXXXXX",
+            storageBucket: "XXXXXXXXXXXXXXXXXXXXXX",
+            messagingSenderId: "XXXXXXXXXXXXXXX",
+            appId: "XXXXXXXXXXXXXXXXXXXXXXX",
+            measurementId: "XXXXXXXXXXXX"
+        };
+
+    }
+    
     return(
         <div>
             <h1>Contact</h1>
