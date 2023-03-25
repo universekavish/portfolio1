df8a9d4397c9f27d9ab1d1462abc544d253bbfdb
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-03-25 17:08:08 +0530

    updating projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,84 +15,48 @@ const Projects = () => {
                             <div className="item col-lg-4 col-md-6">
                                 <div className="icon">
                                     <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/Book-Monk"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
+                                        <a href={"https://github.com/universekavish/portfolio1"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
                                     </i>
                                 </div>
-                                <h3 className="h5">Book Monk</h3>
-                                <p>Building an online book store</p>
+                                <h3 className="h5">My Portfolio</h3>
+                                <p>Building a portfolio to practice React and showcase my skills</p>
                             </div>
                             <div className="item col-lg-4 col-md-6">
                                 <div className="icon">
                                     <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/The-Band"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
+                                        <a href={"https://github.com/universekavish/Prison-Management-System"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
                                     </i>
                                 </div>
-                                <h3 className="h5">The Band</h3>
-                                <p>Fictional Band website for booking tickets for a concert</p>
+                                <h3 className="h5">Prison Management System</h3>
+                                <p>Developed as part of DBMS project. using HTML, CSS, PHP, MySQL</p>
                             </div>
                             <div className="item col-lg-4 col-md-6">
                                 <div className="icon">
                                     <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/Google-Calendar"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
+                                        <a href={"https://github.com/universekavish/Online-Bank-Simulator"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
                                     </i>
                                 </div>
-                                <h3 className="h5">Google Calendar</h3>
-                                <p>Application to manage users google calendar</p>
+                                <h3 className="h5">Online Bank Simulator</h3>
+                                <p>Designed in Java using concepts of Swing, AWT and MySQL.</p>
                             </div>
 
                             <div className="item col-lg-4 col-md-6">
                                 <div className="icon">
                                     <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/exercise-tracker"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
+                                        <a href={"https://github.com/universekavish/IIIT-Kota-Website"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
                                     </i>
                                 </div>
-                                <h3 className="h5">Exercise Tracker</h3>
-                                <p>Building a Exercise Tracker using MERN stack</p>
+                                <h3 className="h5">Institute Website</h3>
+                                <p>First project of Engineering using HTML, CSS, Firebase connection</p>
                             </div>
                             <div className="item col-lg-4 col-md-6">
                                 <div className="icon">
                                     <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/Predictionist"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
+                                        <a href={"https://github.com/universekavish/JSprojects"} ><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
                                     </i>
                                 </div>
-                                <h3 className="h5">Predictionist</h3>
-                                <p>Website for college reviews</p>
-                            </div>
-                            <div className="item col-lg-4 col-md-6">
-                                <div className="icon">
-                                    <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/portfolio"}><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
-                                    </i>
-                                </div>
-                                <h3 className="h5">Portfolio</h3>
-                                <p>Website showcasing my portfolio</p>
-                            </div>
-                            <div className="item col-lg-4 col-md-6">
-                                <div className="icon">
-                                    <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/Weather-App"}><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
-                                    </i>
-                                </div>
-                                <h3 className="h5">Weather App</h3>
-                                <p>Web app displaying current weather</p>
-                            </div>
-                            <div className="item col-lg-4 col-md-6">
-                                <div className="icon">
-                                    <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/movie-serial-info"}><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
-                                    </i>
-                                </div>
-                                <h3 className="h5">Movie Serial Info</h3>
-                                <p>Web app to display information about movies and serials</p>
-                            </div>
-                            <div className="item col-lg-4 col-md-6">
-                                <div className="icon">
-                                    <i className="icon-diploma">
-                                        <a href={"https://github.com/sureshmangs/markdown-previewer"}><img src={githubLogo} alt="logo" style={{ width: "50px", height: "50px" }} /></a>
-                                    </i>
-                                </div>
-                                <h3 className="h5">Markdown Previewer</h3>
-                                <p>React based Markdown Previewer</p>
+                                <h3 className="h5">Different JS projects</h3>
+                                <p>Small JS projects to practice JavaScript</p>
                             </div>
                         </div>
                     </div>
