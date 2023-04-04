fd0553caebbc1023ebcb41d45e3297c6fc3825fa
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2023-04-04 15:53:53 +0530

    removing comment

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App(props) {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
-        {/* <Main /> */}
         <Footer />
       </Router>
     </div>
